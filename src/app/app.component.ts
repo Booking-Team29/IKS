5328c33c3a242405b313ab53c04f157d8ce37b07
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-11-25 17:31:52 +0100

    introduce routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { SearchPageComponent } from './home/search-page/search-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SearchPageComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SearchPageComponent } from './home/search-page/search-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: SearchPageComponent },
+    {path: 'home', component: SearchPageComponent },
+];
