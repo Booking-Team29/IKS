a9a56c0fa7c3d82bef3c566f1ba805ba86a21f54
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-11-25 16:50:16 +0100

    search result card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SearchComponent } from './home/search/search.component';
+import { SearchResultCardComponent } from './home/search-result-card/search-result-card.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SearchComponent, FormsModule],
+  imports: [CommonModule, RouterOutlet, SearchResultCardComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
