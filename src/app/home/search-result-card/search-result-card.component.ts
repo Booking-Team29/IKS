76d3819cd6249d8c3f82c2a391a43438e59d3ef5
Dragoslav Tamindžija <dragodev@protonmail.com>
2023-11-26 10:11:25 +0100

    Home page (#1)
    * search bar
    
    * search result card
    
    * home page
    
    * introduce routing

---
+++ src/app/home/search-result-card/search-result-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search-result-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './search-result-card.component.html',
+  styleUrl: './search-result-card.component.scss'
+})
+export class SearchResultCardComponent {
+
+}
---
+++ src/app/home/search/search.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss'
+})
+export class SearchComponent {
+  destination: string = '';
+  checkInDate: string = '';
+  checkOutDate: string = '';
+  numberOfPeople: number = 1;
+  roomType: string = '';
+
+
+  search() {
+    // TODO: actual logic
+    console.log('Search clicked:', this.destination, this.checkInDate, this.checkOutDate, this.numberOfPeople, this.roomType);
+  }
+}
