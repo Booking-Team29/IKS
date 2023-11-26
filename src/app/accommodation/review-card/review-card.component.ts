bf6002651f20618211722ff1e052d62f88a7cb9b
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-11-26 11:53:50 +0100

    review card

---
+++ src/app/accommodation/review-card/review-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-review-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './review-card.component.html',
+  styleUrl: './review-card.component.scss'
+})
+export class ReviewCardComponent {
+
+}
