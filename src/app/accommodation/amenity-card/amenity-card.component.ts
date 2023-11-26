2a1209ccd24fd6430250df7a530be1ff919f8321
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-11-26 12:07:09 +0100

    amenity card

---
+++ src/app/accommodation/amenity-card/amenity-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-amenity-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './amenity-card.component.html',
+  styleUrl: './amenity-card.component.scss'
+})
+export class AmenityCardComponent {
+
+}
