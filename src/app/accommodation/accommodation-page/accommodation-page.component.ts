ded2cd17a098d6bd7d1aea7b587c5484d26beae4
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-11-26 18:16:32 +0100

    carousel component

--- src/app/accommodation/accommodation-page/accommodation-page.component.ts
+++ src/app/accommodation/accommodation-page/accommodation-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AmenityCardComponent } from '../amenity-card/amenity-card.component';
 import { ReviewCardComponent } from '../review-card/review-card.component';
+import { GalleryCarouselComponent } from '../gallery-carousel/gallery-carousel.component';
 
 @Component({
   selector: 'app-accommodation-page',
   standalone: true,
-  imports: [CommonModule, AmenityCardComponent, ReviewCardComponent],
+  imports: [CommonModule, AmenityCardComponent, ReviewCardComponent, GalleryCarouselComponent],
   templateUrl: './accommodation-page.component.html',
   styleUrl: './accommodation-page.component.scss'
 })
---
+++ src/app/accommodation/gallery-carousel/gallery-carousel.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GalleryModule, GalleryItem, ImageItem } from 'ng-gallery';
+
+@Component({
+  template: `
+    <gallery [items]="images"></gallery>
+  `,
+  selector: 'app-gallery-carousel',
+  standalone: true,
+  imports: [GalleryModule],
+  templateUrl: './gallery-carousel.component.html',
+  styleUrl: './gallery-carousel.component.scss'
+})
+export class GalleryCarouselComponent {
+  images: GalleryItem[] = [];
+
+  ngOnInit() {
+    this.images = [
+      new ImageItem({ src: 'https://www.savills.co.uk/_images/adobestock-539646437.jpg', thumb: 'https://www.savills.co.uk/_images/adobestock-539646437.jpg' }),
+      new ImageItem({ src: 'https://www.savills.co.uk/_images/adobestock-539646437.jpg', thumb: 'https://www.savills.co.uk/_images/adobestock-539646437.jpg' }),
+      new ImageItem({ src: 'https://www.savills.co.uk/_images/adobestock-539646437.jpg', thumb: 'https://www.savills.co.uk/_images/adobestock-539646437.jpg' }),
+    ]
+  }
+}
