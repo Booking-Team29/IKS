433f929b28fb9fea5a9195c1ae6dc45f54a86ed0
teodord25 <djuric.teodor25@gmail.com>
2023-11-27 13:10:35 +0100

    add routes for view and edit profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { SearchPageComponent } from './home/search-page/search-page.component';
 import { AccommodationPageComponent } from './accommodation/accommodation-page/accommodation-page.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 export const routes: Routes = [
     {path: '', component: SearchPageComponent },
     {path: 'home', component: SearchPageComponent },
     {path: 'accommodation/:id', component: AccommodationPageComponent },
+    {path: 'view-profile', component: ViewProfileComponent },
+    {path: 'edit-profile', component: EditProfileComponent },
 ];
