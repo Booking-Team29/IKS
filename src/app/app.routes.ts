505117b4cb6520174b5f089eaee98d0713ce78ea
petar76-2021 <105112524+petar76-2021@users.noreply.github.com>
2023-11-28 16:42:39 +0100

    login and register commponents (#6)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { SearchPageComponent } from './home/search-page/search-page.component';
 import { AccommodationPageComponent } from './accommodation/accommodation-page/accommodation-page.component';
+import {LoginComponent} from "./authentication/login/login.component";
+import {RegisterComponent} from "./authentication/register/register.component";
 
 export const routes: Routes = [
     {path: '', component: SearchPageComponent },
     {path: 'home', component: SearchPageComponent },
     {path: 'accommodation/:id', component: AccommodationPageComponent },
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent}
 ];
