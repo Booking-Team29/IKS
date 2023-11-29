f50888f25920f88351f97928d5ecb613b071eb6f
Teodor Đurić <70525484+teodord25@users.noreply.github.com>
2023-11-29 17:28:54 +0100

    Feature/header (#7)
    * update package things
    
    * implement callback for nav items
    
    * change nav anchors to buttons
    
    * add go home callback to logo
    
    * adjust styling
    
    * add routes for view and edit profile
    
    * login and register commponents (#6)
    
    * add routes for view and edit profile; resolve conflicts
    
    * fix styling inconsistencies
    
    ---------
    
    Co-authored-by: petar76-2021 <105112524+petar76-2021@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
 import { SearchPageComponent } from './home/search-page/search-page.component';
 import { AccommodationPageComponent } from './accommodation/accommodation-page/accommodation-page.component';
-import {LoginComponent} from "./authentication/login/login.component";
-import {RegisterComponent} from "./authentication/register/register.component";
+import { LoginComponent } from "./authentication/login/login.component";
+import { RegisterComponent } from "./authentication/register/register.component";
+import { ViewProfileComponent } from './view-profile/view-profile.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 export const routes: Routes = [
     {path: '', component: SearchPageComponent },
     {path: 'home', component: SearchPageComponent },
     {path: 'accommodation/:id', component: AccommodationPageComponent },
+    {path: 'view-profile', component: ViewProfileComponent },
+    {path: 'edit-profile', component: EditProfileComponent },
     {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent}
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent },
+    {path: 'register', component: RegisterComponent },
+    {path: 'view-profile', component: ViewProfileComponent },
+    {path: 'edit-profile', component: EditProfileComponent },
 ];
