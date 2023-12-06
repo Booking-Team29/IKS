8be0b1eb367e6fcbce122732306724112a66da9a
Teodor Đurić <70525484+teodord25@users.noreply.github.com>
2023-12-06 17:52:13 +0100

    Fix/profile view buttons (#10)
    * fix edit profile button
    
    * fix cancel profile edit button

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountTypeSwitchComponent } from '../account-type-switch/account-type-switch.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-profile',
@@ -13,5 +14,10 @@ export class EditProfileComponent {
   onAccountTypeChange(isOwnerAccount: boolean) {
     console.log('Account type changed:', isOwnerAccount ? 'Owner' : 'Renter');
     // You can handle the change however you need to here
+  }
+
+  constructor(private router: Router) { }
+  cancelEdit() {
+    this.router.navigate(['/view-profile'])
   }
 }
