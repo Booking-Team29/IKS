8f8e9aba7fa826885e99c67bcebcb88f145aafe2
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-12-06 20:01:09 +0100

    add a filter panel to the search page

--- src/app/home/search-page/search-page.component.ts
+++ src/app/home/search-page/search-page.component.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from '../search/search.component';
 import { SearchResultCardComponent } from '../search-result-card/search-result-card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [CommonModule, SearchComponent, SearchResultCardComponent],
+  imports: [CommonModule, SearchComponent, SearchResultCardComponent, FormsModule],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss'
 })
 export class SearchPageComponent {
-
+  lowestRating: number = 5;
+  highestRating: number = 5;
+  hotelChecked: Boolean = false;
+  apartmentChecked: Boolean = false;
+  studioChecked: Boolean = false;
+  parkingChecked: Boolean = false;
+  WiFiChecked: Boolean = false;
+  petChecked: Boolean = false;
+  balconyChecked: Boolean = false;
+  airConditioningChecked: Boolean = false;
 }
