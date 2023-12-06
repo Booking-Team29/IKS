44d1e560f1d9068717119fdc5549e7b8eadd08eb
teodord25 <djuric.teodor25@gmail.com>
2023-12-06 19:22:28 +0100

    implement delete button (add confirmation modal)

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountTypeSwitchComponent } from '../account-type-switch/account-type-switch.component';
+import { ConfirmationModalComponent } from '../confirmation-modal/confirmation-modal.component';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-profile',
   standalone: true,
-  imports: [CommonModule, AccountTypeSwitchComponent],
+  imports: [CommonModule, AccountTypeSwitchComponent, ConfirmationModalComponent],
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.scss'
 })
 export class EditProfileComponent {
+  @ViewChild('confirmationModal') confirmationModal!: ConfirmationModalComponent;
+
   onAccountTypeChange(isOwnerAccount: boolean) {
     console.log('Account type changed:', isOwnerAccount ? 'Owner' : 'Renter');
     // You can handle the change however you need to here
@@ -19,5 +22,9 @@ export class EditProfileComponent {
   constructor(private router: Router) { }
   cancelEdit() {
     this.router.navigate(['/view-profile'])
+  }
+
+  deleteAccount() {
+    console.log('Deleting account...');
   }
 }
