5c37eef52b0b6c6b8ce2da423b1b168c5646cb44
teodord25 <djuric.teodor25@gmail.com>
2023-12-12 10:06:58 +0100

    implement accommodation-creation-requests page component

---
+++ src/app/accommodation/accommodation-creation-requests/accommodation-creation-requests.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApprovalCardComponent } from '../../approval-card/approval-card.component';
+import { Status } from '../accommodation-status.enum';
+
+@Component({
+  selector: 'app-accommodation-creation-requests',
+  standalone: true,
+  imports: [CommonModule, ApprovalCardComponent],
+  templateUrl: './accommodation-creation-requests.component.html',
+  styleUrl: './accommodation-creation-requests.component.scss'
+})
+export class AccommodationCreationRequestsComponent implements OnInit {
+
+  requests!: any[]; // actual requests data type
+  Status = Status;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // initialize requests here, probably fetching from a service
+  }
+
+  onApprove(request: any): void {
+    // handle approve logic, such as updating the request status
+  }
+
+  onDecline(request: any): void {
+    // handle decline logic, such as removing the request or updating status
+  }
+}
+
