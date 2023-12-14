843c09987a6236d15a98fcf38f70186f19c5c4ea
teodord25 <djuric.teodor25@gmail.com>
2023-12-14 05:12:58 +0100

    implement reservation dto

---
+++ src/app/models/reservation-dto.model.ts
@@ -0,0 +1,9 @@
+import { ReservationStatus } from "./reservation-status.enum";
+
+export interface ReservationDTO {
+  startDate: Date;
+  endDate: Date;
+  guestCount: number;
+  status: ReservationStatus;
+  totalPrice: number;
+}
