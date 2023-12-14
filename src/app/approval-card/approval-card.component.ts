828fb88b4a1530c9c399e5131d69a52bbde424b1
teodord25 <djuric.teodor25@gmail.com>
2023-12-14 06:57:09 +0100

    implement and integrate approveRequest

--- src/app/approval-card/approval-card.component.ts
+++ src/app/approval-card/approval-card.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRequestStatus } from '../user-request-status.enum';
 import { Router } from '@angular/router';
+import { AccommodationService } from '../services/accommodation.service';
+import { AccommodationStatus } from '../models/accommodation-status.enum';
+import { ConfirmationModalComponent } from '../confirmation-modal/confirmation-modal.component';
+import { HttpClientModule } from '@angular/common/http';
 
 const classSelectorMap: { [key in UserRequestStatus]: string } = {
     [UserRequestStatus.APPROVED]: 'status-approved',
@@ -14,17 +18,23 @@ const classSelectorMap: { [key in UserRequestStatus]: string } = {
 @Component({
   selector: 'app-approval-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ConfirmationModalComponent, HttpClientModule],
+  providers: [AccommodationService],
   templateUrl: './approval-card.component.html',
   styleUrl: './approval-card.component.scss'
 })
 export class ApprovalCardComponent {
+  @ViewChild('confirmationModal') confirmationModal!: ConfirmationModalComponent;
+
   private _status: UserRequestStatus;
   private _statusClassSelector: string;
   private _name: string;
   private _requestID;
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private service: AccommodationService,
+  ) {
     this._requestID = 1; // impelelemnt :DDDDDDDd
     this._status = UserRequestStatus.CREATED;
     this._statusClassSelector = classSelectorMap[this._status];
@@ -63,8 +73,21 @@ export class ApprovalCardComponent {
   }
 
   approveRequest(requestID: number) {
-    this._status = UserRequestStatus.APPROVED;
-    this._statusClassSelector = classSelectorMap[UserRequestStatus.APPROVED];
+    this.service.get(`${requestID}`).subscribe(response => {
+      let accommodation = response;
+
+      let approvedAccommodationDTO = accommodation;
+      approvedAccommodationDTO.AccommodationStatus = AccommodationStatus.APPROVED;
+
+      this.service.approve(`${requestID}`, approvedAccommodationDTO).subscribe(response => {
+        console.log(response);
+
+        this.confirmationModal.openModal('Successfully approved accommodation creation request!');
+
+        this._status = UserRequestStatus.APPROVED;
+        this._statusClassSelector = classSelectorMap[UserRequestStatus.APPROVED];
+      });
+    });
   }
 
   declineRequest(requestID: number) {
