1bef2a5d00bd4a7ce73ea90ec659adc04473182b
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-12-20 18:34:40 +0100

    getting data from the backend

--- src/app/home/search-page/search-page.component.ts
+++ src/app/home/search-page/search-page.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { SearchComponent } from '../search/search.component';
 import { SearchResultCardComponent } from '../search-result-card/search-result-card.component';
 import { FormsModule } from '@angular/forms';
+import { Accommodation } from '../../models/accommodation.model';
+import { AccommodationDTO } from '../../models/accommodation-dto.model';
 
 @Component({
   selector: 'app-search-page',
@@ -13,10 +15,11 @@ import { FormsModule } from '@angular/forms';
 })
 export class SearchPageComponent {
   lowestRating: number = 5;
-  highestRating: number = 5;
+  highestRating: number = 10;
   hotelChecked: Boolean = false;
   apartmentChecked: Boolean = false;
   studioChecked: Boolean = false;
+
   parkingChecked: Boolean = false;
   WiFiChecked: Boolean = false;
   petChecked: Boolean = false;
@@ -24,4 +27,21 @@ export class SearchPageComponent {
   airConditioningChecked: Boolean = false;
 
   results: any[] = [];
+
+  receiveSearchResults(data: AccommodationDTO[]) {
+    let new_results = []
+    for (const acc of data) {
+      // TODO: rating
+      if (this.hotelChecked && acc.type != "HOTEL") continue;
+      if (this.apartmentChecked && acc.type != "APARTMENT") continue;
+      if (this.studioChecked && acc.type != "STUDIO") continue;
+
+      if (this.WiFiChecked && !acc.amenities.includes("Wi-Fi")) continue;
+      if (this.petChecked && !acc.amenities.includes("Pet")) continue;
+      if (this.balconyChecked && !acc.amenities.includes("Balcony")) continue;
+      if (this.airConditioningChecked && !acc.amenities.includes("AirConditioning")) continue;
+      new_results.push(acc);
+    }
+    this.results = new_results;
+  }
 }
--- src/app/home/search/search.component.ts
+++ src/app/home/search/search.component.ts
@@ -1,21 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AccommodationService } from '../../services/accommodation.service';
+import { AccommodationDTO } from '../../models/accommodation-dto.model';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
+  providers: [AccommodationService],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+  @Output() dataEvent = new EventEmitter<AccommodationDTO[]>()
   destination: string = '';
   checkInDate: string = '';
   checkOutDate: string = '';
   numberOfPeople: number = 1;
 
+  constructor(
+    private service: AccommodationService
+  ) {
+    this.service = service;
+  }
+
 
   search() {
+    let data = this.service.search(
+      this.numberOfPeople, 
+      this.destination, 
+      this.checkInDate, 
+      this.checkOutDate
+      );
+    data.subscribe(
+      data => this.dataEvent.emit(data),
+      error => console.error(error)
+    )
   }
 }
